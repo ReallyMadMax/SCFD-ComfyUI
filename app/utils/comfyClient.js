5b23c8df93e69f28edf66d96ed111d560214cefa
ReallyMadMax <ettrevivre@gmail.com>
2024-12-11 00:15:50 -0500

    again

--- app/utils/comfyClient.js
+++ app/utils/comfyClient.js
@@ -82,7 +82,7 @@ export async function generateImage(userPrompt, serverAddress, width = 768, heig
             class_type: 'CLIPTextEncode',
             inputs: {
                 clip: ['4', 1],
-                text: 'bad hands, ugly, gross, disfigured, lewd, boobs, naked women',
+                text: 'bad hands, ugly, gross, disfigured, lewd, boobs, naked women, cum, sex, innapropriate',
             },
         },
         '8': {
